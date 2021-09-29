b53044442a872f00d7c476688f30997107c3566e
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-09-29 11:09:31 -0300

    Add files via upload

---
+++ comparador-hash.py
@@ -0,0 +1,24 @@
+import hashlib
+
+# arquivos
+arquivo1 = 'a.txt'
+arquivo2 = 'b.txt'
+
+hash1 = hashlib.new('ripemd160')
+
+# modo de leitura no arquivo 1
+hash1.update(open(arquivo1, 'rb').read())
+
+hash2 = hashlib.new('ripemd160')
+
+# modo de leitura no arquivo 2
+hash2.update(open(arquivo2, 'rb').read())
+
+if hash1.digest() != hash2.digest():
+    print(f'O arquivo {arquivo1} é diferente do arquivo {arquivo2}')
+    print(f'O hash do arquivo {arquivo1} é: ', hash1.hexdigest())
+    print(f'O hash do arquivo {arquivo2} é: ', hash2.hexdigest())
+else:
+    print(f'O arquivo {arquivo1} é igual ao arquivo {arquivo2}')
+    print(f'O hash do arquivo {arquivo1} é: ', hash1.hexdigest())
+    print(f'O hash do arquivo {arquivo2} é: ', hash2.hexdigest())
